22e81d262cc7f725829d55677d4baedcf6f892be
sutha <sutha@kurinchilion.com>
2023-05-23 12:43:15 -0400

    say hello app

--- ColorCatalog/App.js
+++ ColorCatalog/App.js
@@ -1,33 +1,44 @@
-import React from "react";
-import { Text, View, StyleSheet } from 'react-native';
-
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const handlePress=()=>{
+    console.log('Hello!')
+  }
   return (
     <View style={styles.container}>
-      <Text style={[styles.paragraph, styles.text]}>
-        Hello World!
-      </Text>
-      <Text style={{fontSize: 12}}>Ready to explore</Text>
+      <Text style={styles.heading}>Hello World!</Text>
+      <TouchableOpacity style={styles.button} onPress={handlePress}>
+        <Text style={styles.buttonText}>Say Hello!</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  heading:{
+    fontSize:28,
+    fontWeight:"700"
+  },  
   container: {
     flex: 1,
+    backgroundColor: '#fe1',
+    alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    backgroundColor: '#fe1'
+  button:{
+    marginTop:50,
+    borderRadius:10,
+    padding:10,
+    backgroundColor: 'crimson',
+    textColor: 'white',
+    width:120,
   },
-  text:{
-    fontSize: 28
+  buttonText:{
+    color:'white',
+    fontSize:20,
+    alignSelf:'center',
+    fontWeight:'500'
   }
 });
--- README.md
+++ README.md
@@ -85,3 +85,50 @@ $ npm start (starts expo application in port 19000 in local environment)
 ```
 npm install react-native-web@~0.18.10, react-dom@18.2.0, @expo/webpack-config@^18.0.1
 ```
+* Simple say hello app to write console.log messages (edit App.js)
+```
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function App() {
+  const handlePress=()=>{
+    console.log('Hello!')
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Hello World!</Text>
+      <TouchableOpacity style={styles.button} onPress={handlePress}>
+        <Text style={styles.buttonText}>Say Hello!</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  heading:{
+    fontSize:28,
+    fontWeight:"700"
+  },  
+  container: {
+    flex: 1,
+    backgroundColor: '#fe1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button:{
+    marginTop:50,
+    borderRadius:10,
+    padding:10,
+    backgroundColor: 'crimson',
+    textColor: 'white',
+    width:120,
+  },
+  buttonText:{
+    color:'white',
+    fontSize:20,
+    alignSelf:'center',
+    fontWeight:'500'
+  }
+});
+```
