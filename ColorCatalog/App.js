364cd2ce95845541b19b5c4de39c416ae503bf14
sutha <sutha@kurinchilion.com>
2023-05-23 20:23:42 -0400

    adding data capture via form

--- ColorCatalog/App.js
+++ ColorCatalog/App.js
@@ -1,18 +1,27 @@
 import React, {useState} from "react";
-import { StyleSheet, View  } from 'react-native';
-import Color from "./components/ColorButton";
+import { StyleSheet, FlatList, Alert  } from 'react-native';
+import defaultColors from "./data/defaultColors.json";
+import ColorButton from "./components/ColorButton";
+import ColorForm from "./components/ColorForm";
 
 export default function App() {
 
   const [backgroundColor, setBackgroundColor] = useState("white");
 
   return (
-    <View style={[styles.container, {backgroundColor}]}>
-      <Color backgroundColor="red" onPress={setBackgroundColor}/>
-      <Color backgroundColor="green" onPress={setBackgroundColor}/>
-      <Color backgroundColor="blue" onPress={setBackgroundColor}/>
-      <Color backgroundColor="purple" onPress={setBackgroundColor}/>
-    </View>
+    <>
+      <ColorForm onNewColor={ (newColor) => {
+        Alert.alert(`new color entered ${newColor}`);
+      }}/>
+      <FlatList style={[styles.container, {backgroundColor}]} 
+        data={defaultColors}
+        renderItem={({item}) => {
+          return (
+            <ColorButton key={item.id} backgroundColor={item.color} onPress={setBackgroundColor} />
+          );
+        }}
+        />
+    </>
   );
 }
 
@@ -20,30 +29,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
   },
-  button:{
-    margin:10,
-    padding: 10,
-    borderWidth: 2,
-    borderRadius: 10,
-    alignSelf: "stretch",
-    backgroundColor: "rgba(255, 255, 255, .8)"
-  },
-  buttonText:{
-    fontSize: 30,
-    textAlign: "center"
-  },
-  sample:{
-    width: 20,
-    height: 20,
-    borderRadius: 10,
-    backgroundColor: "white",
-    margin: 5,
-  },
-  row:{
-    flexDirection: "row",
-    alignItems: "center"
-  }
 });
---
+++ ColorCatalog/components/ColorForm.js
@@ -0,0 +1,48 @@
+import React, {useState, useRef} from 'react';
+import {StyleSheet, View, TextInput, Button} from "react-native";
+
+function ColorForm({onNewColor = f => f}) {
+    const [inputValue, setValue ] = useState("");
+    const input = useRef();
+
+    console.log(" -> ", inputValue);
+    return (
+        <View style={styles.container}>
+            <TextInput 
+                ref={input}
+                style={styles.txtInput} 
+                value={inputValue}
+                autoCapitalize="none"
+                onChangeText={setValue}
+                placeholder='Enter a color ...'
+                />
+            <Button 
+                title="Add" 
+                onPress={ () => {
+                    input.current.blur();
+                    console.log(`Add value ${inputValue}`);
+                    onNewColor(inputValue);
+                    setValue("");
+            }}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 40,
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    txtInput:{
+        flex: 1,
+        borderWidth: 2,
+        fontSize: 20,
+        margin: 4,
+        borderRadius: 5,
+        padding: 4
+    }
+  });
+
+  export default ColorForm;
